61393327446359986be9e4567f6a9a69eb39edf3
Rob Schafer <rob@schafer.net>
2012-02-25 17:21:41 -0600

    Moved Rails app to top level.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NolahealthMobile::Application.config.secret_token = 'bc89baaef682c26f0bad73e3a03e94268242906e92f4e8186a0ee0d44832770d6275411fb5fe5d01e28e893b901160fb748f32b45409125ff73489a7fbd07c69'
